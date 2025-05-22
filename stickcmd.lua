14d480f73f7fc458c00d833522bdbab833f15817
Leif <leif>
2025-05-22 19:07:32 -0400

    change around filled rectangle drawing

--- stickcmd.lua
+++ stickcmd.lua
@@ -60,33 +60,39 @@ end
 
 local function drawStick(col, top, w, h, code)
   if string.len(code) == 1 then
-	if code == "U" then
-	  lcd.drawFilledRectangle(col + w / 2 - 1, top + 2, 3, h / 2 - 4)
-	elseif code == "D" then
-	  lcd.drawFilledRectangle(col + w / 2 - 1, top + h / 2 + 2, 3, h / 2 - 4)
-	elseif code == "L" then
-	  lcd.drawFilledRectangle(col + 2, top + h / 2 - 1, w / 2 - 4, 3)
-	elseif code == "R" then
-	  lcd.drawFilledRectangle(col + w / 2 + 4, top + h / 2 - 1, w / 2 - 4, 3)
+    local R = nil
+    if code == "U" then
+      R = {x = col + w / 2 - 1, y = top + 2, w = 3, h = h / 2 - 4}
+    elseif code == "D" then
+      R = {x = col + w / 2 - 1, y = top + h / 2 + 2, w = 3, h = h / 2 - 4}
+    elseif code == "L" then
+      R = {x = col + 2, y = top + h / 2 - 1, w = w / 2 - 4, h = 3}
+    elseif code == "R" then
+      R = {x = col + w / 2 + 4, y = top + h / 2 - 1, w = w / 2 - 4, h = 3}
     elseif code == "C" then
-	  lcd.drawFilledRectangle(col + w / 2 - 2, top + h / 2 - 2, 5, 5)
-	end
+      R = {x = col + w / 2 - 2, y = top + h / 2 - 2, w = 5, h = 5}
+    end
+    if R ~= nil then
+      lcd.drawFilledRectangle(R["x"], R["y"], R["w"], R["h"])
+    else
+      print("bad stick code " .. code)
+    end
   else
     local L = nil
     if code == "UL" then
-	  L = {x1 = col + 2, y1 = top + 2, x2 = col + w / 2 - 4, y2 = top + h / 2 - 4}
-	elseif code == "UR" then
-	  L = {x1 = col + w - 2, y1 = top + 2, x2 = col + w / 2 + 4, y2 = top + h / 2 - 4}
-	elseif code == "DR" then
-	  L = {x1 = col + w - 2, y1 = top + h - 2, x2 = col + w / 2 + 4, y2 = top + h / 2 + 4}
-	elseif code == "DL" then
-	  L = {x1 = col + 2, y1 = top + h - 2, x2 = col + w / 2 - 4, y2 = top + h / 2 + 4}
-	end
-	if L ~= nil then
-	  lcd.drawLine(L["x1"], L["y1"], L["x2"], L["y2"], SOLID, INVERS)
-	else
-	  print("bad stick code " .. code)
-	end
+      L = {x1 = col + 2, y1 = top + 2, x2 = col + w / 2 - 4, y2 = top + h / 2 - 4}
+    elseif code == "UR" then
+      L = {x1 = col + w - 2, y1 = top + 2, x2 = col + w / 2 + 4, y2 = top + h / 2 - 4}
+    elseif code == "DR" then
+      L = {x1 = col + w - 2, y1 = top + h - 2, x2 = col + w / 2 + 4, y2 = top + h / 2 + 4}
+    elseif code == "DL" then
+      L = {x1 = col + 2, y1 = top + h - 2, x2 = col + w / 2 - 4, y2 = top + h / 2 + 4}
+    end
+    if L ~= nil then
+      lcd.drawLine(L["x1"], L["y1"], L["x2"], L["y2"], SOLID, INVERS)
+    else
+      print("bad stick code " .. code)
+    end
   end
 end
 
