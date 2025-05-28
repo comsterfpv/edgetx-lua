33383c89958ec07301101e36106467bd9869a89f
Leif <leif>
2025-05-28 14:25:36 -0400

    combine stick command groups into one table

--- TOOLS/stickcmd.lua
+++ TOOLS/stickcmd.lua
@@ -11,44 +11,35 @@ local w = nil
 local h = nil
 local label_row = nil
 
-commands_fc = { -- these are good in both Betaflight and INAV
-  {"Calibrate ACC", "UL", "D"},
-  {"Calibrate Gyro", "DL", "D"},
-  {"Calibrate Compass", "UR", "D"},
-  {"Profile 1", "DL", "L"},
-  {"Profile 2", "DL", "U"},
-  {"Profile 3", "DL", "R"},
-  {"Trim ACC Left", "U", "L"},
-  {"Trim ACC Right", "U", "R"},
-  {"Trim ACC Forwards", "U", "U"},
-  {"Trim ACC Backwards", "U", "D"},
-  {"Enter OSD Menu", "L", "U"},
-  {"Save Settings", "DL", "DR"},
+commands = { -- FC commands are valid in both Betaflight and INAV
+  {"FC", "Calibrate ACC", "UL", "D"},
+  {"FC", "Calibrate Gyro", "DL", "D"},
+  {"FC", "Calibrate Compass", "UR", "D"},
+  {"FC", "Profile 1", "DL", "L"},
+  {"FC", "Profile 2", "DL", "U"},
+  {"FC", "Profile 3", "DL", "R"},
+  {"FC", "Trim ACC Left", "U", "L"},
+  {"FC", "Trim ACC Right", "U", "R"},
+  {"FC", "Trim ACC Forwards", "U", "U"},
+  {"FC", "Trim ACC Backwards", "U", "D"},
+  {"FC", "Enter OSD Menu", "L", "U", "ifosdm"},
+  {"FC", "Save Settings", "DL", "DR"},
+  {"INAV", "Bat Profile 1", "UL", "L"},
+  {"INAV", "Bat Profile 2", "UL", "U"},
+  {"INAV", "Bat Profile 3", "UL", "R"},
+  {"INAV", "Load Waypoint Mission", "D", "UR", "iflwpm"},
+  {"INAV", "Save Waypoint Mission", "D", "UL"},
+  {"INAV", "Unload Waypoint Mission", "D", "DR"},
+  {"HDZero", "VTX Menu", "DR", "DL"},
+  {"HDZero", "Camera Menu", "R", "C"},
+  {"HDZero", "Pit Mode", "UL", "UR"}, -- this is OPT_B, check on OPT_A
 }
 
-commands_inav = {
-  {"Bat Profile 1", "UL", "L"},
-  {"Bat Profile 2", "UL", "U"},
-  {"Bat Profile 3", "UL", "R"},
-  {"Load Waypoint Mission", "D", "UR"},
-  {"Save Waypoint Mission", "D", "UL"},
-  {"Unload Waypoint Mission", "D", "DR"},
-}
-
-commands_hdzero = {
-  {"VTX Menu", "DR", "DL"},
-  {"Camera Menu", "R", "C"},
-  {"Pit Mode", "UL", "UR"}, -- this is OPT_B, check on OPT_A
-}
-
-groups = {
-  {"FC", commands_fc},
-  {"INAV", commands_inav},
-  {"HDZero", commands_hdzero},
-}
-
-local function drawHeader()
-end
+-- groups = {
+--   {"FC", commands_fc},
+--   {"INAV", commands_inav},
+--   {"HDZero", commands_hdzero},
+-- }
 
 local function drawGimbal(col, top, w, h)
   lcd.drawRectangle(col, top, w, h)
@@ -96,6 +87,44 @@ local function drawStick(col, top, w, h, code)
   end
 end
 
+local function channelOutput(code)
+  if string.len(code) == 1 then
+    local R = nil
+    if code == "U" then
+      return 0, 1 -- R = {x = col + w / 2 - 1, y = top + 2, w = 3, h = h / 2 - 4}
+    elseif code == "D" then
+      return 0, -1 -- R = {x = col + w / 2 - 1, y = top + h / 2 + 2, w = 3, h = h / 2 - 4}
+    elseif code == "L" then
+      return -1, 0 -- R = {x = col + 2, y = top + h / 2 - 1, w = w / 2 - 4, h = 3}
+    elseif code == "R" then
+      return 1, 0 -- R = {x = col + w / 2 + 4, y = top + h / 2 - 1, w = w / 2 - 4, h = 3}
+    elseif code == "C" then
+      return 0, 0 -- R = {x = col + w / 2 - 2, y = top + h / 2 - 2, w = 5, h = 5}
+    end
+    if R ~= nil then
+      -- lcd.drawFilledRectangle(R["x"], R["y"], R["w"], R["h"])
+    else
+      print("bad stick code " .. code)
+    end
+  else
+    local L = nil
+    if code == "UL" then
+      return -1, 1 -- L = {x1 = col + 2, y1 = top + 2, x2 = col + w / 2 - 4, y2 = top + h / 2 - 4}
+    elseif code == "UR" then
+      return 1, 1 -- L = {x1 = col + w - 2, y1 = top + 2, x2 = col + w / 2 + 4, y2 = top + h / 2 - 4}
+    elseif code == "DR" then
+      return 1, -1 -- L = {x1 = col + w - 2, y1 = top + h - 2, x2 = col + w / 2 + 4, y2 = top + h / 2 + 4}
+    elseif code == "DL" then
+      return -1, -1 -- L = {x1 = col + 2, y1 = top + h - 2, x2 = col + w / 2 - 4, y2 = top + h / 2 + 4}
+    end
+    if L ~= nil then
+      -- lcd.drawLine(L["x1"], L["y1"], L["x2"], L["y2"], SOLID, INVERS)
+    else
+      print("bad stick code " .. code)
+    end
+  end
+end
+
 local function drawStickCommand(code1, code2)
   drawGimbal(left_col, top, w, h)
   drawStick(left_col, top, w, h, code1)
@@ -104,12 +133,14 @@ local function drawStickCommand(code1, code2)
 end
 
 local function drawState()
-  local group_tuple = groups[group_index]
-  group_str = group_tuple[1]
-  command_dict = groups[group_index][2]
-  command_str = command_dict[command_index][1]
-  command_code1 = command_dict[command_index][2]
-  command_code2 = command_dict[command_index][3]
+  -- local group_tuple = groups[group_index]
+  -- group_str = group_tuple[1]
+  -- command_dict = groups[group_index][2]
+  command_dict = commands[command_index]
+  group_str = command_dict[1]
+  command_str = command_dict[2]
+  command_code1 = command_dict[3]
+  command_code2 = command_dict[4]
   drawStickCommand(command_code1, command_code2)
   lcd.drawText(1, label_row, command_str)
   lcd.drawText(1, 0, "Stick Cmd: " .. group_str, INVERS)
@@ -157,7 +188,7 @@ local function identifySpecialFunc()
   end
 end
 
-local function handleEvent(event)
+local function handleEventOld(event)
   if event == EVT_VIRTUAL_PREV then
     command_index = command_index - 1
     if command_index <= 0 then
@@ -181,10 +212,24 @@ local function handleEvent(event)
   end
 end
 
+local function handleEvent(event)
+  if event == EVT_VIRTUAL_PREV then
+    command_index = command_index - 1
+    if command_index <= 0 then
+      command_index = #commands
+    end
+  elseif event == EVT_VIRTUAL_NEXT then
+    command_index = command_index + 1
+    if command_index > #commands then
+      command_index = 1
+    end
+  end
+end
+
 -- Init
 local function init()
-  group_count = #groups
-  group_index = 1
+  -- group_count = #groups
+  -- group_index = 1
   command_index = 1
   left_col = 10
   right_col = 60
